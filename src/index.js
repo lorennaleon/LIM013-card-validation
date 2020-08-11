9ef948e2f28fcb5d16b1ad8f0fd6ed85cf5de4dd
lorennalehu <showbells0702gmail.com>
2020-08-11 16:22:53 -0500

    codigo final

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import validator from './validator.js';
-
-console.log(validator);
+import validator from './validator.js';76
+//click funcion al boton validar
+document.getElementById('btn-validar').addEventListener('click',function(){
+  //llamo a la caja de tarjeta de credito del html
+  let numero =document.getElementById('inputNumero').value;
+  //llamo al validador isvalid de validator.js 
+  let resultado=validator.isValid(numero);
+  //ocultar
+  let numeroOculto =validator.maskify(numero);
+  if(resultado==true){
+    document.getElementById('resultado').innerHTML+= numeroOculto + " "+ "es válida";
+    
+  } else{
+    document.getElementById('resultado').innerHTML+= numeroOculto + " "+ "es inválida";
+  }
+});
+  
